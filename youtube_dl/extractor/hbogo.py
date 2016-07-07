5e625090dae2f9bfb64811d9451b6e747a0ca296
James Bove <jebove@ucsc.edu>
2016-07-07 13:41:08 -0700

    started making tests & title regex

--- youtube_dl/extractor/hbogo.py
+++ youtube_dl/extractor/hbogo.py
@@ -4,30 +4,63 @@ from __future__ import unicode_literals
 from .common import InfoExtractor
 
 
-class YourExtractorIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?yourextractor\.com/watch/(?P<id>[0-9]+)'
-    _TEST = {
-        'url': 'http://yourextractor.com/watch/42',
-        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
-        'info_dict': {
-            'id': '42',
-            'ext': 'mp4',
-            'title': 'Video title goes here',
-            'thumbnail': 're:^https?://.*\.jpg$',
-            # TODO more properties, either as:
-            # * A value
-            # * MD5 checksum; start the string with md5:
-            # * A regular expression; start the string with re:
-            # * Any Python type (for example int or float)
+class HBOGOIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:play\.)?hbogo\.com/(?P<type>(episode|season|extra))/urn:hbo:(?P=type):(?P<id>[a-zA-Z0-9]+)'
+    _TESTS = [
+        {
+            'url': 'http://play.hbogo.com/extra/urn:hbo:extra:GV3QgNQ8DXFvCwgEAAAAa',
+            'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
+            'info_dict': {
+                'id': 'GVtcUtwHMXoiyw9oIAAAG',
+                'ext': 'mp4',
+                'title': 'Trending Clip: John Oliver on Independence Day',
+                'thumbnail': 're:^https?://.*\.jpg$',
+                # TODO more properties, either as:
+                # * A value
+                # * MD5 checksum; start the string with md5:
+                # * A regular expression; start the string with re:
+                # * Any Python type (for example int or float)
+            }
         }
-    }
+        {
+            'url': 'http://play.hbogo.com/episode/urn:hbo:episode:GVtcUtwHMXoiyw9oIAAAG',
+            'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
+            'info_dict': {
+                'id': 'GVtcUtwHMXoiyw9oIAAAG',
+                'ext': 'mp4',
+                'title': 'Ep 1   The Red Woman',
+                'thumbnail': 're:^https?://.*\.jpg$',
+                # TODO more properties, either as:
+                # * A value
+                # * MD5 checksum; start the string with md5:
+                # * A regular expression; start the string with re:
+                # * Any Python type (for example int or float)
+            }
+        }
+        {
+            # Playlist (season) test--very very unfinished (more so than the other 2)
+            'url': 'http://play.hbogo.com/season/urn:hbo:season:GVtcUtwKx_pfCwuUIAAAG',
+            'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
+            'info_dict': {
+                'id': 'GVtcUtwKx_pfCwuUIAAAG',
+                'ext': 'mp4',
+                'title': 'Game of Thrones',
+                'thumbnail': 're:^https?://.*\.jpg$',
+                # TODO more properties, either as:
+                # * A value
+                # * MD5 checksum; start the string with md5:
+                # * A regular expression; start the string with re:
+                # * Any Python type (for example int or float)
+            }
+        }
+    ]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
         # TODO more code goes here, for example ...
-        title = self._html_search_regex(r'<h1>(.+?)</h1>', webpage, 'title')
+        title = self._html_search_regex(r'<span style="font-family: Gotham_6r[^"]*"[^>]*>\s*(.+?)\s*</span>', webpage, 'title')
 
         return {
             'id': video_id,
